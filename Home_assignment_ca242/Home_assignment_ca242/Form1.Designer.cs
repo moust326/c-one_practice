136d9b77d882c6684ea51c350997c233feea5916
moust326 <moustapheaabdirazakhassan@gmail.com>
2026-09-23 23:15:50 +0300

    Home_assignment_ca242

---
+++ Home_assignment_ca242/Home_assignment_ca242/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Home_assignment_ca242
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_day_of_the_week = new System.Windows.Forms.Label();
+            this.lblnameofthemonth = new System.Windows.Forms.Label();
+            this.lblnumericofthemonth = new System.Windows.Forms.Label();
+            this.lbloutput = new System.Windows.Forms.Label();
+            this.lblentertheyear = new System.Windows.Forms.Label();
+            this.txtdayoftheweek = new System.Windows.Forms.TextBox();
+            this.txtnameofthemonth = new System.Windows.Forms.TextBox();
+            this.txtnumberofmonth = new System.Windows.Forms.TextBox();
+            this.txtyear = new System.Windows.Forms.TextBox();
+            this.btnshowdate = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnclose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_day_of_the_week
+            // 
+            this.lbl_day_of_the_week.AutoSize = true;
+            this.lbl_day_of_the_week.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_day_of_the_week.Location = new System.Drawing.Point(147, 33);
+            this.lbl_day_of_the_week.Name = "lbl_day_of_the_week";
+            this.lbl_day_of_the_week.Size = new System.Drawing.Size(273, 64);
+            this.lbl_day_of_the_week.TabIndex = 0;
+            this.lbl_day_of_the_week.Text = "enter day of the week:\r\n\r\n";
+            // 
+            // lblnameofthemonth
+            // 
+            this.lblnameofthemonth.AutoSize = true;
+            this.lblnameofthemonth.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnameofthemonth.Location = new System.Drawing.Point(108, 92);
+            this.lblnameofthemonth.Name = "lblnameofthemonth";
+            this.lblnameofthemonth.Size = new System.Drawing.Size(312, 32);
+            this.lblnameofthemonth.TabIndex = 0;
+            this.lblnameofthemonth.Text = "enter name of the month:\r\n";
+            // 
+            // lblnumericofthemonth
+            // 
+            this.lblnumericofthemonth.AutoSize = true;
+            this.lblnumericofthemonth.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumericofthemonth.Location = new System.Drawing.Point(30, 143);
+            this.lblnumericofthemonth.Name = "lblnumericofthemonth";
+            this.lblnumericofthemonth.Size = new System.Drawing.Size(390, 32);
+            this.lblnumericofthemonth.TabIndex = 0;
+            this.lblnumericofthemonth.Text = "enter the numeric of the month:\r\n";
+            // 
+            // lbloutput
+            // 
+            this.lbloutput.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lbloutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbloutput.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbloutput.Location = new System.Drawing.Point(74, 269);
+            this.lbloutput.Name = "lbloutput";
+            this.lbloutput.Size = new System.Drawing.Size(877, 75);
+            this.lbloutput.TabIndex = 0;
+            this.lbloutput.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblentertheyear
+            // 
+            this.lblentertheyear.AutoSize = true;
+            this.lblentertheyear.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblentertheyear.Location = new System.Drawing.Point(234, 199);
+            this.lblentertheyear.Name = "lblentertheyear";
+            this.lblentertheyear.Size = new System.Drawing.Size(186, 32);
+            this.lblentertheyear.TabIndex = 0;
+            this.lblentertheyear.Text = "enter the year:\r\n";
+            // 
+            // txtdayoftheweek
+            // 
+            this.txtdayoftheweek.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtdayoftheweek.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtdayoftheweek.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdayoftheweek.Location = new System.Drawing.Point(426, 26);
+            this.txtdayoftheweek.Name = "txtdayoftheweek";
+            this.txtdayoftheweek.Size = new System.Drawing.Size(525, 39);
+            this.txtdayoftheweek.TabIndex = 1;
+            // 
+            // txtnameofthemonth
+            // 
+            this.txtnameofthemonth.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtnameofthemonth.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtnameofthemonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnameofthemonth.Location = new System.Drawing.Point(426, 85);
+            this.txtnameofthemonth.Name = "txtnameofthemonth";
+            this.txtnameofthemonth.Size = new System.Drawing.Size(525, 39);
+            this.txtnameofthemonth.TabIndex = 1;
+            // 
+            // txtnumberofmonth
+            // 
+            this.txtnumberofmonth.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtnumberofmonth.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtnumberofmonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnumberofmonth.Location = new System.Drawing.Point(426, 141);
+            this.txtnumberofmonth.Name = "txtnumberofmonth";
+            this.txtnumberofmonth.Size = new System.Drawing.Size(525, 39);
+            this.txtnumberofmonth.TabIndex = 1;
+            // 
+            // txtyear
+            // 
+            this.txtyear.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtyear.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtyear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtyear.Location = new System.Drawing.Point(426, 199);
+            this.txtyear.Name = "txtyear";
+            this.txtyear.Size = new System.Drawing.Size(525, 39);
+            this.txtyear.TabIndex = 1;
+            // 
+            // btnshowdate
+            // 
+            this.btnshowdate.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnshowdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnshowdate.Location = new System.Drawing.Point(202, 377);
+            this.btnshowdate.Name = "btnshowdate";
+            this.btnshowdate.Size = new System.Drawing.Size(180, 91);
+            this.btnshowdate.TabIndex = 2;
+            this.btnshowdate.Text = "show date";
+            this.btnshowdate.UseVisualStyleBackColor = false;
+            this.btnshowdate.Click += new System.EventHandler(this.btnshowdate_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnclear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclear.Location = new System.Drawing.Point(445, 377);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(180, 91);
+            this.btnclear.TabIndex = 2;
+            this.btnclear.Text = "clear";
+            this.btnclear.UseVisualStyleBackColor = false;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btnclose
+            // 
+            this.btnclose.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnclose.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclose.Location = new System.Drawing.Point(689, 377);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(179, 91);
+            this.btnclose.TabIndex = 2;
+            this.btnclose.Text = "close";
+            this.btnclose.UseVisualStyleBackColor = false;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1136, 558);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnshowdate);
+            this.Controls.Add(this.txtyear);
+            this.Controls.Add(this.txtnumberofmonth);
+            this.Controls.Add(this.txtnameofthemonth);
+            this.Controls.Add(this.txtdayoftheweek);
+            this.Controls.Add(this.lblentertheyear);
+            this.Controls.Add(this.lbloutput);
+            this.Controls.Add(this.lblnumericofthemonth);
+            this.Controls.Add(this.lblnameofthemonth);
+            this.Controls.Add(this.lbl_day_of_the_week);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_day_of_the_week;
+        private System.Windows.Forms.Label lblnameofthemonth;
+        private System.Windows.Forms.Label lblnumericofthemonth;
+        private System.Windows.Forms.Label lbloutput;
+        private System.Windows.Forms.Label lblentertheyear;
+        private System.Windows.Forms.TextBox txtdayoftheweek;
+        private System.Windows.Forms.TextBox txtnameofthemonth;
+        private System.Windows.Forms.TextBox txtnumberofmonth;
+        private System.Windows.Forms.TextBox txtyear;
+        private System.Windows.Forms.Button btnshowdate;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnclose;
+    }
+}
+
---
+++ Home_assignment_ca242/Home_assignment_ca242/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Home_assignment_ca242
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnshowdate_Click(object sender, EventArgs e)
+        {
+            //stage1 of input
+            //creating variables
+            String day_of_the_week, name_of_the_month,  full_date;
+            int numeric_day, year;
+            //initial values
+            day_of_the_week = txtdayoftheweek.Text;
+            name_of_the_month = txtnameofthemonth.Text;
+            //type casting using parse
+            numeric_day = int.Parse(txtnumberofmonth.Text);
+            year =int.Parse( txtyear.Text);
+            //stage2 process concatination of full date
+            full_date = day_of_the_week + "," + name_of_the_month + "," + numeric_day + "," + year;
+            //stage3 the output using label
+            lbloutput.Text = full_date;
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            //clear textbox and label
+            txtdayoftheweek.Clear();
+            txtnumberofmonth.Text = "";
+            txtnameofthemonth.Text = string.Empty;
+            txtyear.Clear();
+            //clearing the output
+            lbloutput.Text = "";
+
+        }
+
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            //form close using this keyword and close functinon
+            this.Close();
+        }
+    }
+}
